a0f9c7fbab6e984cbe594de64b29c3460b564123
Statika3k <shahanova.ns@gmail.com>
2025-09-22 18:10:32 +0300

    Создан новый компонент user-posts-page-component.js

---
+++ components/user-posts-page-component.js
@@ -0,0 +1,74 @@
+import { renderHeaderComponent } from "./header-component.js";
+import { posts, goToPage, user } from "../index.js";
+
+/**
+ * @TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
+ * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
+ */
+export function renderUserPostsPageComponent({ appEl, userId }) {
+  const userPosts = posts.filter((post) => post.user.id === userId);
+
+  if (userPosts.length === 0) {
+    appEl.innerHTML = `
+      <div class="page-container">
+        <div class="header-container"></div>
+        <div class="no-posts">
+          <p>У этого пользователя пока нет постов.</p>
+        </div>
+      </div>
+    `;
+  } else {
+    // Генерируем HTML для каждого поста пользователя
+    const postsHtml = userPosts
+      .map(
+        (post) =>
+          `
+    <li class="post" data-post-id="${post.id}">
+            <div class="post-header" data-user-id="${post.user.id}">
+              <img src="${post.user.imageUrl}" class="post-header__user-image">
+              <p class="post-header__user-name">${post.user.name}</p>
+            </div>
+            <div class="post-image-container">
+              <img class="post-image" src="${post.imageUrl}">
+            </div>
+            <div class="post-likes">
+              <button data-post-id="${post.id}" class="like-button">
+                <img src="./assets/images/like-not-active.svg">
+              </button>
+              <p class="post-likes-text">
+                Нравится: <strong>${post.likes.length}</strong>
+              </p>
+            </div>
+            <p class="post-text">
+              <span class="user-name">${post.user.name}</span>
+              ${post.description}
+            </p>
+            <p class="post-date">
+              ${post.addLater}
+            </p>
+          </li>
+        `
+      )
+      .join("");
+
+    appEl.innerHTML = `
+    <div class="page-container">
+      <div class="header-container"></div>
+      <ul class="posts">
+        ${postsHtml}          
+      </ul>
+    </div>`;
+  }
+
+  renderHeaderComponent({
+    element: document.querySelector(".header-container"),
+  });
+
+  for (let userEl of document.querySelectorAll(".post-header")) {
+    userEl.addEventListener("click", () => {
+      goToPage(USER_POSTS_PAGE, {
+        userId: userEl.dataset.userId,
+      });
+    });
+  }
+}
