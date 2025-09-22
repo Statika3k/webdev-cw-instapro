68b5be94adcdb60ed94a11b1dd5db130376177e3
Statika3k <shahanova.ns@gmail.com>
2025-09-22 11:24:31 +0300

    Интеграция верстки списка постов с API

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -2,22 +2,18 @@ import { USER_POSTS_PAGE } from "../routes.js";
 import { renderHeaderComponent } from "./header-component.js";
 import { posts, goToPage } from "../index.js";
 
-export function renderPostsPageComponent({ appEl, posts }) {
-  // @TODO: реализовать рендер постов из api
+export function renderPostsPageComponent({ appEl }) {
   console.log("Актуальный список постов:", posts);
 
   /**
    * @TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
    * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
    */
-  const appHtml = `
-  <div class="page-container">
-    <div class="header-container"></div>
-    <ul class="posts">
-      ${posts
-        .map((post) => {
-          return `
-          <li class="post">
+  const postsHtml = posts
+    .map(
+      (post) =>
+        `
+    <li class="post" data-post-id="${post.id}">
             <div class="post-header" data-user-id="${post.user.id}">
               <img src="${post.user.imageUrl}" class="post-header__user-image">
               <p class="post-header__user-name">${post.user.name}</p>
@@ -41,9 +37,15 @@ export function renderPostsPageComponent({ appEl, posts }) {
               ${post.addLater}
             </p>
           </li>
-        `;
-        })
-        .join("")}
+        `
+    )
+    .join("");
+
+  const appHtml = `
+  <div class="page-container">
+    <div class="header-container"></div>
+    <ul class="posts">
+      ${postsHtml}          
     </ul>
   </div>`;
 
